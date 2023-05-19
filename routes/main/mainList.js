633a8c41741276153fee8bc5196d60717efb8ec1
KROomoO <parksungik00@naver.com>
2023-05-19 17:38:58 +0900

    add : server

---
+++ routes/main/mainList.js
@@ -0,0 +1,36 @@
+const mainListMapper = require("mybatis-mapper");
+
+mainListMapper.createMapper(["./models/mybatis/main/mainListMapper.xml"]);
+
+let format = { language: "sql", indent: " " };
+
+const mysql = require("@config/mysql/db");
+
+const mainList = {
+    list: function (req, res) {
+        const param = req.body;
+        const query = mainListMapper.getStatement(
+            "listMapper",
+            "loadGunameQuery",
+            param,
+            format
+        );
+        mysql.query(query, (error, result) => {
+            res.json(result);
+        });
+    },
+    guname: function (req, res) {
+        const param = req.query;
+        const query = mainListMapper.getStatement(
+            "listMapper",
+            "loadSelectListQuery",
+            param,
+            format
+        );
+        mysql.query(query, (error, result) => {
+            res.json(result);
+        });
+    },
+};
+
+module.exports = mainList;
