b4a30380073a858f3b8c1ef4405635fc88b3e900
KROomoO <pwkc00@gmail.com>
2023-11-24 16:49:19 +0900

    qwer

--- config/mysql/db.js
+++ config/mysql/db.js
@@ -1,14 +1,5 @@
 const mysql = require("mysql");
 
-// const connection = mysql.createPool({
-//     host: "34.64.217.13",
-//     port: '3306',
-//     user: "root",
-//     password: "qkrtjddlr00!",
-//     database: "evento",
-//     multipleStatements: true
-// });
-
 const connection = mysql.createPool({
     host: process.env.db_host,
     port: process.env.db_port,
